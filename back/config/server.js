8b42923d337ee461b080c4edb4c94bc9be925164
Rémi de Juvigny <remi@hey.com>
2020-08-10 12:21:29 +0200

    Set up project

---
+++ back/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'de55556a80ead58c742101dc34b14902'),
+    },
+  },
+});
---
+++ back/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '973ed769-a6f6-4885-adef-4bdd3a2f0397'
+};
